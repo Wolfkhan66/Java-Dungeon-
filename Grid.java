832adbc2fb21ed1191866779105fd0c15807e384
Wolfkhan66 <cailehwald@googlemail.com>
2016-01-28 00:39:59 +0000

    moved collision detection to grid class, something is now happening but collision is buggy

--- Grid.java
+++ Grid.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
+import java.awt.Rectangle;
 public class Grid {
 
     public int centerX, centerY, speedX, speedY;
     public ArrayList<Grid> grid = new ArrayList<Grid>();
     public Player player = StartingClass.getplayer();
- 
+     private Rectangle r;
     // Behavioral Methods
     public void update() {
          centerX += speedX;
@@ -23,6 +24,37 @@ public class Grid {
             centerY += player.getSpeedY(); //This changes centerY by adding speedY.
         }
         
+         r = new Rectangle();
+
+        r.setBounds(centerX, centerY, 50, 50);
+
+        if (r.intersects(Player.CollisionZone)) {
+            checkVerticalCollision(Player.Top, Player.Bottom);
+            checkSideCollision(Player.Left, Player.Right);
+        }
+    }
+    public void checkVerticalCollision(Rectangle rtop, Rectangle rbot){
+        
+        if (rtop.intersects(r)) {
+            player.setSpeedY(0);
+            player.setCenterY(centerY + 102);
+        }
+        if (rbot.intersects(r)) {
+            player.setSpeedY(0);
+            player.setCenterY(centerY - 63);
+        }
+    }
+    public void checkSideCollision(Rectangle rleft, Rectangle rright) {
+        if (rleft.intersects(r)) {
+            player.setCenterX(centerX + 102);
+            player.setSpeedX(0);
+
+        }
+        if (rright.intersects(r)) {
+            player.setCenterX(centerX - 62);
+            player.setSpeedX(0);
+        }
+
     }
 
     public int getCenterX() {
--- Tile.java
+++ Tile.java
@@ -3,7 +3,6 @@ public class Tile extends Grid
 {
     // instance variables - replace the example below with your own
 
-    private Rectangle r;
 
     /**
      * Constructor for objects of class Tile
@@ -14,37 +13,6 @@ public class Tile extends Grid
         setCenterX(centerX);
         setCenterY(centerY);
 
-        r = new Rectangle();
-
-        r.setBounds(centerX, centerY, 50, 50);
-
-        if (r.intersects(Player.CollisionZone)) {
-            checkVerticalCollision(Player.Top, Player.Bottom);
-            checkSideCollision(Player.Left, Player.Right);
-        }
-
     }
-    public void checkVerticalCollision(Rectangle rtop, Rectangle rbot){
-        
-        if (rtop.intersects(r)) {
-            player.setSpeedY(0);
-            player.setCenterY(centerY + 102);
-        }
-        if (rbot.intersects(r)) {
-            player.setSpeedY(0);
-            player.setCenterY(centerY - 63);
-        }
-    }
-    public void checkSideCollision(Rectangle rleft, Rectangle rright) {
-        if (rleft.intersects(r)) {
-            player.setCenterX(centerX + 102);
-            player.setSpeedX(0);
 
-        }
-        if (rright.intersects(r)) {
-            player.setCenterX(centerX - 62);
-            player.setSpeedX(0);
-        }
-
-    }
 }
